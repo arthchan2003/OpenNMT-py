44377e54169c59b89398cb69bc532d13903b2485
Arthur Chan <arthchan2003@yahoo.com>
2018-04-24 18:19:52 -0400

    Added fix to make the codebase works for python 2.7.

--- onmt/Loss.py
+++ onmt/Loss.py
@@ -121,7 +121,7 @@ class LossComputeBase(nn.Module):
 
         for shard in shards(shard_state, shard_size):
             loss, stats = self._compute_loss(batch, **shard)
-            loss.div(normalization).backward()
+            loss.div(normalization).backward(retain_graph=True)
             batch_stats.update(stats)
 
         return batch_stats
@@ -138,10 +138,13 @@ class LossComputeBase(nn.Module):
         """
         pred = scores.max(1)[1]
         non_padding = target.ne(self.padding_idx)
-        num_correct = pred.eq(target) \
-                          .masked_select(non_padding) \
-                          .sum()
-        return onmt.Statistics(loss[0], non_padding.sum(), num_correct)
+        num_correct = pred.eq(target).masked_select(non_padding).long().sum()
+                          
+                          
+#        return onmt.Statistics(loss[0], non_padding.sum(), num_correct)
+
+        return onmt.Statistics(loss.cpu().numpy(), non_padding.long().sum(), num_correct)
+        #return onmt.Statistics(loss[0], non_padding.long().sum(), num_correct)
 
     def _bottle(self, v):
         return v.view(-1, v.size(2))
